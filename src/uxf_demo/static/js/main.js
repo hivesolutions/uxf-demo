486f107c196f80bb218d7065a34fbb779b68e9d3
joamag <joamag@gmail.com>
2015-01-05 15:50:20 +0000

    new date values (2015)

--- src/uxf_demo/static/js/main.js
+++ src/uxf_demo/static/js/main.js
@@ -1,5 +1,5 @@
 ﻿// Hive Colony Framework
-// Copyright (C) 2008-2014 Hive Solutions Lda.
+// Copyright (C) 2008-2015 Hive Solutions Lda.
 //
 // This file is part of Hive Colony Framework.
 //
@@ -20,7 +20,7 @@
 // __version__   = 1.0.0
 // __revision__  = $LastChangedRevision$
 // __date__      = $LastChangedDate$
-// __copyright__ = Copyright (c) 2008-2014 Hive Solutions Lda.
+// __copyright__ = Copyright (c) 2008-2015 Hive Solutions Lda.
 // __license__   = GNU General Public License (GPL), Version 3
 
 // the style to be used from the
